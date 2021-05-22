8d089a3673df39bdccfacd553c17f605ae7becc4
Ravi Kumar <ravikumarshah078@gmail.com>
2021-05-22 09:09:28 +0530

    after modified

--- practice/interfaces/Fighter.java
+++
@@ -1,8 +0,0 @@
-package interfaces;
-
-public interface Fighter {
-	
-	String name1="Fighter";
-	public void fight();
-
-}
--- practice/interfaces/Machanic.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-// 	interfces is the abstract class automatically.
-// and it can't be initialized here and all the methods are abstract also.
-public interface Machanic {
-
-	String name = "Machanic";
-	public void fix();
-}
